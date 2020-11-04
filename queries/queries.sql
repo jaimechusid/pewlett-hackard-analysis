2216add0ea29057a43b6653718c244387de24684
jaimechusid <jaimech@umich.edu>
2020-11-03 19:05:49 -0500

    Deliverable 1

--- queries/queries.sql
+++ queries/queries.sql
@@ -38,3 +38,116 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info ri
+LEFT JOIN dept_emp de ON ri.emp_no = de.emp_no;
+
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info ri
+LEFT JOIN dept_emp de ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+select * from retirement_dept;
+
+select * from salaries
+order by to_date desc;
+
+select e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.gender, 
+	s.salary, 
+	de.to_date
+into emp_info
+from employees e
+inner join salaries s on s.emp_no = e.emp_no
+inner join dept_emp de on de.emp_no = e.emp_no
+where (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+and (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name, 
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager dm
+INNER JOIN departments d ON d.dept_no = dm.dept_no
+INNER JOIN current_emp ce ON ce.emp_no = dm.emp_no;
+	
+select ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+into dept_info
+from current_emp ce
+inner join dept_emp de on de.emp_no = ce.emp_no
+inner join departments d on d.dept_no = de.dept_no;
+
+select ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+from current_emp ce
+inner join dept_emp de on de.emp_no = ce.emp_no
+inner join departments d on d.dept_no = de.dept_no
+where d.dept_name = 'Sales';
+
+select ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+from current_emp ce
+inner join dept_emp de on de.emp_no = ce.emp_no
+inner join departments d on d.dept_no = de.dept_no
+where d.dept_name in ('Sales', 'Development');
